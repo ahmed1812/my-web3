37e8b05384a430bb7ec4ccb74c7db935ac80ae08
ahmed ahmed <algandel19@gmail.com>
2020-03-24 11:18:00 -0500

    json

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.post('/email', (req, res) => {
         // setup email data with unicode symbols
         let mailOptions = {
             from: '" my profoleo "<aLgandel86@gmail.com>', // sender address
-            to: 'aLgandel86@gmail.com', // list of receivers
+            to: 'aLgandel19@gmail.com', // list of receivers
 
 
             subject: 'Node Contact Request web2', // Subject line
@@ -67,9 +67,7 @@ app.post('/email', (req, res) => {
         });
     });
 });
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-}
+
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, "client", "index.html"))
 });
