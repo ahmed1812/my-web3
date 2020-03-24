2df142b85e4c49061ddc54dfbcec6fe4e61f5294
ahmed ahmed <algandel19@gmail.com>
2020-03-24 00:40:37 -0500

    test

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ app.post('/email', (req, res) => {
     });
 });
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client'));
+    app.use(express.static('client/build'));
 }
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, "client", "index.html"))
